96a39b296e927fbbbe6d8072d4eda3459623c984
ZHANG HUANG <vision2best@gmail.com>
2023-06-30 10:08:23 +0800

    graceful

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/bytedance/sonic v1.9.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
--- main.go
+++ main.go
@@ -1,34 +1,83 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/EDDYCJY/go-gin-example/pkg/logging"
 	"github.com/EDDYCJY/go-gin-example/pkg/setting"
 	"github.com/EDDYCJY/go-gin-example/routers"
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	gin.SetMode(setting.RunMode)
 
-	routersInit := routers.InitRouter()
-	readTimeout := setting.ReadTimeout
-	writeTimeout := setting.WriteTimeout
-	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
-	maxHeaderBytes := 1 << 20
+	router := routers.InitRouter()
+
 	s := &http.Server{
-		Addr:           endPoint,
-		Handler:        routersInit,
-		ReadTimeout:    readTimeout,
-		WriteTimeout:   writeTimeout,
-		MaxHeaderBytes: maxHeaderBytes,
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
 
-	logging.Info("[info] start http server listening %s", endPoint)
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
 
-	err := s.ListenAndServe()
-	if err != nil {
-		return
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
+
+	log.Println("Server exiting")
+
+	//endless.DefaultReadTimeOut = setting.ReadTimeout
+	//endless.DefaultWriteTimeOut = setting.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
+	//endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	//
+	//server := endless.NewServer(endPoint, routers.InitRouter())
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
+	//}
+	//
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err: %v", err)
+	//}
+
+	//gin.SetMode(setting.RunMode)
+	//
+	//routersInit := routers.InitRouter()
+	//readTimeout := setting.ReadTimeout
+	//writeTimeout := setting.WriteTimeout
+	//endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	//maxHeaderBytes := 1 << 20
+	//s := &http.Server{
+	//	Addr:           endPoint,
+	//	Handler:        routersInit,
+	//	ReadTimeout:    readTimeout,
+	//	WriteTimeout:   writeTimeout,
+	//	MaxHeaderBytes: maxHeaderBytes,
+	//}
+	//
+	//logging.Info("[info] start http server listening %s", endPoint)
+	//
+	//err := s.ListenAndServe()
+	//if err != nil {
+	//	return
+	//}
 }
